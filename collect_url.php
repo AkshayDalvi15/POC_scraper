dcb90eb8f693175c7262452c81b20233226a06cf
Akshay Dalvi <akshaydalvi212@gmail.com>
2022-11-04 18:42:46 +0530

    added require comments

--- collect_url.php
+++ collect_url.php
@@ -3,14 +3,9 @@
 require_once './gz_file_extraction.php';
 
 // $output_file = './extracted_files/sitemap_dp_Business_Centre.xml';
+// $output_file comes from the gz_file_extraction.php
 
 $xmldata = simplexml_load_file($output_file);
-
-// var_dump($xmldata);
-// exit();
-
-// echo $xmldata->url[1]->loc;
-// exit();
 $urls = [];
 
 foreach ($xmldata->url as $test) {
